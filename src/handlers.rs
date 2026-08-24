1c4dc21c71fa004717152e25e3c616a14c4449a2
mac <@b.tunde.ipaye@gmail.com>
2026-08-24 19:57:19 +0100

    feat: handlers updated commit by ENGR. IPAYE

---
+++ src/handlers.rs
@@ -0,0 +1,7 @@
+use axum::{ extract ::{ path, query, state }, json};
+use crate::models::{ TdmbResponse, VideoResponse, PageQuery, SearchQuery };
+use crate::state::AppState;
+
+pub async fn root() -> &'static str {
+    "Netflix Backend is online"
+}
