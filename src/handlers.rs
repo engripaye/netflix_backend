dc15746072830fb0f8762f2b5acd75689a45d1b1
mac <@b.tunde.ipaye@gmail.com>
2026-08-24 19:58:59 +0100

    refractor: handlers updated commit by ENGR. IPAYE

--- src/handlers.rs
+++ src/handlers.rs
@@ -3,5 +3,5 @@ use crate::models::{ TdmbResponse, VideoResponse, PageQuery, SearchQuery };
 use crate::state::AppState;
 
 pub async fn root() -> &'static str {
-    "Netflix Backend is online"
+    "Netflix Backend is online!"
 }
